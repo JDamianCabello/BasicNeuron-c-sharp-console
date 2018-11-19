0e443dfa1d6858eb108d7704fb6889bab7993794
Damián Cabello <jdamiancabello@gmail.com>
2018-11-19 20:29:42 +0100

    Movida la neurona a su clase individual.

---
+++ RedNeuronalBasica/Neurona.cs
@@ -0,0 +1,36 @@
+﻿namespace RedNeuronalBasica
+{
+    public class Neurona
+    {
+        public double[] Pesos { get; set; }
+        public double Umbral { get; set; }
+
+        public Neurona(double[] pesos, double umbral)
+        {
+            Pesos = pesos;
+            Umbral = umbral;
+        }
+
+        public double Salida(double[] entradas)
+        {
+            return FalsaSigmoide(neurona(entradas));
+        }
+
+        double neurona(double[] entradas)
+        {
+            double sumaAux = 0;
+
+            for (int i = 0; i < entradas.Length; i++)
+                sumaAux += entradas[i] * Pesos[i];
+
+            sumaAux += Umbral;
+
+            return sumaAux;
+        }
+
+        double FalsaSigmoide(double d)
+        {
+            return d > 0 ? 1 : 0;
+        }
+    }
+}
--- RedNeuronalBasica/Program.cs
+++ RedNeuronalBasica/Program.cs
@@ -6,66 +6,55 @@ namespace RedNeuronalBasica
     {
         static void Main(string[] args)
         {
-            Program program = new Program();
+            Neurona neurona = new Neurona(new double[2],0);
 
-            double peso1, peso2; //Esto es la cantidad de entradas de nuestra neurona, en nuestro caso 2.
 
-            double umbral;   //El umbral define la diferencia de 0
-
-            /*
-             *  Neurona creada a través de un AND lógico y dos entradas
-             *  
-             *  
-             *       E1   E2   AND
-             *       0     0    0
-             *       0     1    0
-             *       1     0    0
-             *       1     1    1
-             *      
-             */
+         
 
             Random rnd = new Random();
+
+            //El error de nuestra neurona es: Lo que necesitamos - lo que tenemos
             bool bandera = false;
 
             while (!bandera)
             {
                 bandera = true;
-                peso1 = rnd.NextDouble() - rnd.NextDouble();
-                peso2 = rnd.NextDouble() - rnd.NextDouble();
-                umbral = rnd.NextDouble() - rnd.NextDouble();
+                neurona.Pesos[0] = rnd.NextDouble() - rnd.NextDouble();
+                neurona.Pesos[1] = rnd.NextDouble() - rnd.NextDouble();
+                neurona.Umbral = rnd.NextDouble() - rnd.NextDouble();
 
                 Console.WriteLine("".PadRight(Console.BufferWidth, '-'));
-                Console.WriteLine("Peso 1: {0}", peso1);
-                Console.WriteLine("Peso 2: {0}", peso2);
-                Console.WriteLine("Umbral: {0}", umbral);
+                Console.WriteLine("Peso 1: {0}", neurona.Pesos[0]);
+                Console.WriteLine("Peso 2: {0}", neurona.Pesos[1]);
+                Console.WriteLine("Umbral: {0}", neurona.Umbral);
 
 
-                Console.WriteLine("E1:1     E2:1   :   {0}", program.funcionNeurona(program.Neurona(1, 1, peso1, peso2, umbral)));
+                Console.WriteLine("E1:1     E2:1   :   {0}", neurona.Salida(new double[2] { 1, 1 }));
 
-                Console.WriteLine("E1:1     E2:0   :   {0}", program.funcionNeurona(program.Neurona(1, 0, peso1, peso2, umbral)));
+                Console.WriteLine("E1:1     E2:0   :   {0}", neurona.Salida(new double[2] { 1, 0 }));
 
-                Console.WriteLine("E1:0     E2:1   :   {0}", program.funcionNeurona(program.Neurona(0, 1, peso1, peso2, umbral)));
+                Console.WriteLine("E1:0     E2:1   :   {0}", neurona.Salida(new double[2] { 0, 1 }));
 
-                Console.WriteLine("E1:0     E2:0   :   {0}", program.funcionNeurona(program.Neurona(0, 0, peso1, peso2, umbral)));
+                Console.WriteLine("E1:0     E2:0   :   {0}", neurona.Salida(new double[2] { 0, 0 }));
 
 
-                if (program.funcionNeurona(program.Neurona(1, 1, peso1, peso2, umbral)) != 1)
+                if (neurona.Salida(new double[2] { 1, 1 }) != 1)
                 {
                     bandera = false;
                 }
 
 
-                if (program.funcionNeurona(program.Neurona(1, 0, peso1, peso2, umbral)) != 0)
+                if (neurona.Salida(new double[2] { 1, 0 }) != 0)
                 {
                     bandera = false;
                 }
 
-                if (program.funcionNeurona(program.Neurona(0, 1, peso1, peso2, umbral)) != 0)
+                if (neurona.Salida(new double[2] { 0, 1 }) != 0)
                 {
                     bandera = false;
                 }
 
-                if (program.funcionNeurona(program.Neurona(0, 0, peso1, peso2, umbral)) != 0)
+                if (neurona.Salida(new double[2] { 0, 0 }) != 0)
                 {
                     
                     bandera = false;
