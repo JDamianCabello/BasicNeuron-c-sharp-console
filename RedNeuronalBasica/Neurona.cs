8f3ece8ad52768a136634a3ad72a9d9ca86ec89f
Damián Cabello <jdamiancabello@gmail.com>
2018-11-19 21:33:52 +0100

    Añadida la funcionalidad de aprender a la neurona, en función de una tasa de error. En este caso es fija de 0.3. Solo necesita el total de entradas para iniciarse

--- RedNeuronalBasica/Neurona.cs
+++ RedNeuronalBasica/Neurona.cs
@@ -2,13 +2,58 @@
 {
     public class Neurona
     {
-        public double[] Pesos { get; set; }
+        public double[] EntradasNeurona { get; set; }
         public double Umbral { get; set; }
 
-        public Neurona(double[] pesos, double umbral)
+        private const double TASADEAPRENDIZAJE = 0.3;
+        //Corresponde al valor que aprende cada vez que falla, valores mas altos
+        //dan resultados más rápidos pero menos exactos.
+
+        private double[] entradasAnteriores;
+        double umbralAnterior;
+
+        public Neurona(int nEntradas)
         {
-            Pesos = pesos;
-            Umbral = umbral;
+            EntradasNeurona = new double[nEntradas];
+            entradasAnteriores = new double[nEntradas];
+            Aprender();
+        }
+
+        /// <summary>
+        /// Se ejecuta al crear la neurona, la pone a aprender automáticamente.
+        /// </summary>
+        public void Aprender()
+        {
+            System.Random rnd = new System.Random();
+
+            for (int i = 0; i < entradasAnteriores.Length; i++)
+                entradasAnteriores[i] = rnd.NextDouble() - rnd.NextDouble();
+
+            umbralAnterior = rnd.NextDouble() - rnd.NextDouble();
+
+            EntradasNeurona = entradasAnteriores;
+            Umbral = umbralAnterior;
+
+        }
+
+        /// <summary>
+        /// En caso de error de la neurona se corrige con este método
+        /// </summary>
+        /// <param name="entradas">Las entradas que ha recibido la neurona</param>
+        /// <param name="salidaEsperada">La salida que debería haber tenido la misma</param>
+        public void Aprender(double[] entradas, double salidaEsperada)
+        {
+            //El error de nuestra neurona es: Lo que necesitamos - lo que tenemos
+            double error = salidaEsperada - Salida(entradas);
+
+            for (int i = 0; i < EntradasNeurona.Length; i++)
+                EntradasNeurona[i] = entradasAnteriores[i] + TASADEAPRENDIZAJE * error * entradas[i];
+
+            Umbral = umbralAnterior + TASADEAPRENDIZAJE * error;
+
+            entradasAnteriores = EntradasNeurona;
+            umbralAnterior = Umbral;
+
         }
 
         public double Salida(double[] entradas)
@@ -16,18 +61,28 @@
             return FalsaSigmoide(neurona(entradas));
         }
 
+        /// <summary>
+        /// Método que calcula el valor total de las entradas de la neurona. 
+        /// </summary>
+        /// <param name="entradas">Los valores de entradas de la neurona</param>
+        /// <returns>suma de las entradas multiplicadas por sus pesos, más el umbral</returns>
         double neurona(double[] entradas)
         {
             double sumaAux = 0;
 
             for (int i = 0; i < entradas.Length; i++)
-                sumaAux += entradas[i] * Pesos[i];
+                sumaAux += entradas[i] * EntradasNeurona[i];
 
             sumaAux += Umbral;
 
             return sumaAux;
         }
 
+        /// <summary>
+        /// Método que simula la formula matemática sigmoide. No está ajustado
+        /// </summary>
+        /// <param name="d"></param>
+        /// <returns></returns>
         double FalsaSigmoide(double d)
         {
             return d > 0 ? 1 : 0;
--- RedNeuronalBasica/Program.cs
+++ RedNeuronalBasica/Program.cs
@@ -6,57 +6,45 @@ namespace RedNeuronalBasica
     {
         static void Main(string[] args)
         {
-            Neurona neurona = new Neurona(new double[2],0);
+            Neurona neurona = new Neurona(2);
 
-
-         
-
-            Random rnd = new Random();
-
-            //El error de nuestra neurona es: Lo que necesitamos - lo que tenemos
             bool bandera = false;
 
             while (!bandera)
             {
                 bandera = true;
-                neurona.Pesos[0] = rnd.NextDouble() - rnd.NextDouble();
-                neurona.Pesos[1] = rnd.NextDouble() - rnd.NextDouble();
-                neurona.Umbral = rnd.NextDouble() - rnd.NextDouble();
-
                 Console.WriteLine("".PadRight(Console.BufferWidth, '-'));
-                Console.WriteLine("Peso 1: {0}", neurona.Pesos[0]);
-                Console.WriteLine("Peso 2: {0}", neurona.Pesos[1]);
+                Console.WriteLine("Peso 1: {0}", neurona.EntradasNeurona[0]);
+                Console.WriteLine("Peso 2: {0}", neurona.EntradasNeurona[1]);
                 Console.WriteLine("Umbral: {0}", neurona.Umbral);
-
-
                 Console.WriteLine("E1:1     E2:1   :   {0}", neurona.Salida(new double[2] { 1, 1 }));
-
                 Console.WriteLine("E1:1     E2:0   :   {0}", neurona.Salida(new double[2] { 1, 0 }));
-
                 Console.WriteLine("E1:0     E2:1   :   {0}", neurona.Salida(new double[2] { 0, 1 }));
-
                 Console.WriteLine("E1:0     E2:0   :   {0}", neurona.Salida(new double[2] { 0, 0 }));
 
 
                 if (neurona.Salida(new double[2] { 1, 1 }) != 1)
                 {
+                    neurona.Aprender(new double[2] { 1, 1 }, 1);
                     bandera = false;
                 }
 
 
                 if (neurona.Salida(new double[2] { 1, 0 }) != 0)
                 {
+                    neurona.Aprender(new double[2] { 1, 0 }, 0);
                     bandera = false;
                 }
 
                 if (neurona.Salida(new double[2] { 0, 1 }) != 0)
                 {
+                    neurona.Aprender(new double[2] { 0, 1 }, 0);
                     bandera = false;
                 }
 
                 if (neurona.Salida(new double[2] { 0, 0 }) != 0)
                 {
-                    
+                    neurona.Aprender(new double[2] { 0, 0 }, 0);
                     bandera = false;
                 }
 
